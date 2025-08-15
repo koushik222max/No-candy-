cd3a32455777ee6cabe39656d5887056a3169914
Koushik Raj <koushikraj768@gmail.com>
2025-08-15 18:56:09 +0530

    Add files via upload

---
+++ no candy program.py
@@ -0,0 +1,15 @@
+for i in range(1,11):
+    if i==3:
+        print(f'hey person {i} no cany for you')
+
+    else:
+        print(f'hey person {i} today is my birthday')
+
+print('')
+
+for i in range(1,11):
+    if i!=3:
+        print(f'hey firend {i} today is my bithday')
+
+    else:
+        print(f'hey fiend {i} no candy for you')
